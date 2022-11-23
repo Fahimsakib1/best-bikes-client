14ef26d4e7fd2a80ca43cd2bceb674b8ef68b1da
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-11-24 02:54:49 +0600

    added gallery section on home page

--- src/Pages/Home/Brands/Brands.js
+++ src/Pages/Home/Brands/Brands.js
@@ -8,7 +8,18 @@ import pic5 from '../../../images/Brand-Bikes/yamaha.jpeg';
 const Brands = () => {
     return (
         <div className='my-24'>
-            
+            <h2 className='md:text-5xl lg:text-5xl font-semibold text-blue-600 sm:text-4xl text-3xl text-center'>Gallery</h2>
+
+            <section className="py-6  text-gray-50">
+                <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4 ">
+                    <img src= {pic4} alt="" className="w-full h-full col-span-2 row-span-2 rounded-md shadow-sm min-h-96 md:col-start-3 md:row-start-1 bg-gray-500 aspect-square" />
+                    <img alt="" className="w-full h-full rounded-md shadow-sm min-h-48 bg-gray-500 aspect-square" src={pic2} />
+                    <img alt="" className="w-full h-full rounded-md shadow-sm min-h-48 bg-gray-500 aspect-square" src={pic3} />
+                    <img alt="" className="w-full h-full rounded-md shadow-sm min-h-48 bg-gray-500 aspect-square" src={pic1} />
+                    <img alt="" className="w-full h-full rounded-md shadow-sm min-h-48 bg-gray-500 aspect-square" src={pic5} />
+                    
+                </div>
+            </section>
         </div>
     );
 };
